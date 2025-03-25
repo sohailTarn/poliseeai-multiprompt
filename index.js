522c968fbe69ec406f148dc9578b075894602562
sohailPlero <sohail@plero.co>
2025-03-25 15:06:50 +0800

    updated prompt

--- index.js
+++ index.js
@@ -172,7 +172,8 @@ app.post('/answer-question', async (req, res) => {
 
     Example 3:
     *Does the client hold any regulatory licence? If so please state this
-    If the client holds a regulatory license explicitly mentioned in the document (a distribution license does not count as a regulatory license. We are explicitly looking for a regulatory license) Answer: Yes, the clients policy does have a regulatory license  **Reference licence** **Target Document Section Reference**
+    Important note for this question: If the client holds a regulatory license explicitly mentioned in the document (a distribution license does not count as a regulatory license. We are explicitly looking for a regulatory license. So look explicitly for regulatory licesne number. It has to be regulatory license number no any other license number) 
+    Answer: Yes, the clients policy does have a regulatory license  **Reference licence** **Target Document Section Reference**
     Status: Compliant
 
     If the client DOES NOT hold a regulatory license explicitly mentioned in the document Answer: No, the clients policy does have a regulatory license  **Reference licence** **Section XX**. If the document mentions any other licese mention it here.
